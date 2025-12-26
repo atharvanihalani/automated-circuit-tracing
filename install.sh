07b38aad5c38d594e9d34b616bec923eabece777
root <atharvanihalani@gmail.com>
2025-12-26 00:19:32 +0000

    added test for topological sort

--- install.sh
+++ install.sh
@@ -4,4 +4,8 @@ source ~/.bashrc
 uv venv
 source .venv/bin/activate
 uv pip install -e circuit-tracer/
-uv pip install dotenv bs4
+uv pip install dotenv bs4 hf_transfer matplotlib
+export HF_HUB_ENABLE_HF_TRANSFER=1
+
+curl -fsSL https://claude.ai/install.sh | bash
+export IS_SANDBOX=1
